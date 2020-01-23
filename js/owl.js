fd07f154a314ca82869520a41381b0120c6fbcd2
arun5309 <avr5309@gmail.com>
2020-01-23 15:27:19 +0530

    UI improvements

--- js/owl.js
+++ js/owl.js
@@ -1,17 +1,19 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
-    loop:true,
     autoplay: true,
+    center: true,
+    touchDrag: true,
+    autoWidth: true,
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:1
+            items:3
         },
         1000:{
-            items:1
+            items:5
         }
     }
 })
